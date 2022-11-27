b765082ea5b1a11fb1f5712e7217fa18e9b6f336
usernameee <user32234@gmail.com>
2022-11-27 21:35:09 +0400

    Add project files.

---
+++ ViewSQLConnection/ViewForm.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ViewForm.aspx.cs" Inherits="ViewSQLConnection.ViewForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="ViewForm.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblCategoryName" runat="server" Text="Category Name" CssClass ="label"></asp:Label>
+            <asp:TextBox ID="txtCategoryName" runat="server" CssClass="txtBox"></asp:TextBox>
+        </div>
+        <asp:Label ID="lblCheck" runat="server" Text="" CssClass ="lblCheck"></asp:Label>
+        <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click" CssClass="button" />
+        <asp:GridView ID="grdView" runat="server" CssClass ="grdView">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ ViewSQLConnection/ViewForm.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ViewSQLConnection
+{
+    public partial class ViewForm : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection("Data Source=localhost;Initial Catalog=Northwind;Integrated Security=True");
+        SqlCommand cmd;
+        DataTable DT = new DataTable();
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new SqlCommand("DataSearch", conn);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@CatgName", txtCategoryName.Text);
+            cmd.Parameters.Add(new SqlParameter()
+            {
+                Direction = ParameterDirection.Output,
+                ParameterName = "@finalvalue",
+                SqlDbType = SqlDbType.Int
+            });
+            cmd.ExecuteNonQuery();
+            if (cmd.Parameters["@finalvalue"].Value.ToString() == "1")
+            {
+                SqlDataAdapter DA = new SqlDataAdapter(cmd);
+                DA.Fill(DT);
+                grdView.DataSource = DT;
+                grdView.DataBind();
+                lblCheck.Visible = false;
+            }
+            
+            else 
+            {
+                lblCheck.Visible = true;
+                lblCheck.Text = "THERE IS NO DATA SUCH AS !!!";
+            }
+            
+            conn.Close();
+        }
+    }
+}
